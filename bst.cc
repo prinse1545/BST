f21db98b9dc88d19185ea12416c5fc0387d5a4cc
prinse1545 <philipp.msrivastava@gmail.com>
2019-10-17 15:45:13 -0400

    implementation of some methods

--- bst.cc
+++ bst.cc
@@ -25,11 +25,63 @@ bool BST<T>::empty() const {
 template <class T>
 T* BST<T>::get(const T &k) const {
   //Phil
+  if(empty()) throw new EmptyBSTException;
+
+  Node* curr = root;
+
+  while(curr != NULL && k != curr->val) {
+
+    if(k < curr->val) {
+      curr = curr->left;
+    }
+    else {
+      curr = curr->right;
+    }
+  }
+
+  return &curr->val;
 }
 
 template <class T>
 void BST<T>::insert(T *k) {
   //Phil
+
+  Node* z = new Node;
+
+  z->parent = NULL;
+  z->right = NULL;
+  z->left = NULL;
+  z->val = *k;
+
+  Node* curr = root;
+  Node* y = NULL;
+
+  while(curr != NULL) {
+    y = curr;
+
+    if(z->val < curr->val) {
+      curr = curr->left;
+    }
+    else {
+      curr = curr->right;
+    }
+  }
+
+  z->parent = y;
+  if(y == NULL) {
+    root = z;
+  }
+  else if(z->val < y->val) {
+    y->left = z;
+  }
+  else {
+    y->right = z;
+  }
+
+  count++;
+
+  cout << count << endl;
+
 }
 
 template <class T>
@@ -40,16 +92,33 @@ void BST<T>::remove(const T &k) {
 template <class T>
 T* BST<T>::maximum() const {
   //Phil
+  Node* curr = root;
+
+  while(curr->right != NULL) {
+    curr = curr->right;
+  }
+
+  return curr;
 }
 
 template <class T>
 T* BST<T>::minimum() const {
   //Phil
+
+  Node* curr = root;
+
+  while(curr->left != NULL) {
+    curr = curr->left;
+  }
+
+  return curr;
 }
 
 template <class T>
 T* BST<T>::successor(const T &k) const {
   //Phil
+
+
 }
 
 template <class T>
@@ -59,7 +128,14 @@ T* BST<T>::predecessor(const T &k) const {
 
 template <class T>
 string BST<T>::inOrder() const {
-  //Phil
+
+  Node* curr = root;
+
+  if(curr != NULL) {
+    inOrder(curr->left);
+    cout << curr->val;
+    inOrder(curr->right);
+  }
 }
 
 template <class T>
--- test_bst.cc
+++ test_bst.cc
@@ -5,7 +5,15 @@ using namespace std;
 
 int main() {
 
-  BST<int> bs();
+  BST<int> bs;
+
+  int to_insert = 9;
+  int* p;
+
+  bs.insert(&to_insert);
+  // p = bs.get(to_insert);
+  //
+  // cout << *p << endl;
 
   return 0;
 }
